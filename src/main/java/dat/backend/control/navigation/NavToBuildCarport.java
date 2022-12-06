0889c648ecac32131aaa8aad255a37ddf3fd657d
FeliciaFavrholdt <feliciafavrholdt93@gmail.com>
2022-12-06 15:07:51 +0100

    adds nav servlets

---
+++ src/main/java/dat/backend/control/navigation/NavToBuildCarport.java
@@ -0,0 +1,23 @@
+package dat.backend.control.navigation;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "navToBuildCarport", value = "/navToBuildCarport")
+public class NavToBuildCarport extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // You shouldn't end up here with a GET-request, thus you get sent back to frontpage
+        response.sendRedirect("index.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+
+    }
+}
